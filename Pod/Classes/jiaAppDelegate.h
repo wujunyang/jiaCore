ba08bd2445d5cfe00b640daa409834e306c86aac
wujunyang <wujunyang@126.com>
2016-06-21 15:42:01 +0800

    RJ

---
+++ Pod/Classes/jiaAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  jiaAppDelegate.h
+//  HHLog
+//
+//  Created by wujunyang on 16/6/21.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface jiaAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
