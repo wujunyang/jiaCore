ff3cbce034e2d9aab7fe3466adbcc092db8593db
wujunyang <wujunyang@126.com>
2016-06-23 17:17:30 +0800

    sdfsdf

---
+++ Pod/UmengCore/XAspect-UmengAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  XAspect-UmengAppDelegate.h
+//  testGt
+//
+//  Created by wujunyang on 16/6/22.
+//  Copyright © 2016年 wujunyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface XAspect_UmengAppDelegate : NSObject
+
+@end
